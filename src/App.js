9d542cb927fbbd85c06bb674a5eb09272dd08a6e
Nikhil Rawal <rawal.nikhil29@gmail.com>
2021-09-27 23:25:36 -0700

    change3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ExternalFrame from "./myScripts/ExternalFrame"
 
-const dotenv = require('dotenv').config()
 function App() {
   return (
     <div className="App">
